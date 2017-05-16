8641e644377ddf33361f8ff55d9f18772fabe793
Geoff Dutton <g.dutton@gmail.com>
2017-05-16 17:43:37 -0500

    not sure this is the best solution, how often does uBlock restart?

--- platform/safari/vapi-background.js
+++ platform/safari/vapi-background.js
@@ -717,6 +717,7 @@ vAPI.setIcon = function(tabId, iconStatus, badge) {
                 iconStateForTabId[curTabId].active = true;
             }
             vAPI.tabs.stack[curTabId] = curTab;
+            vAPI.tabs.reload(curTabId);
         }
     }
 })();
